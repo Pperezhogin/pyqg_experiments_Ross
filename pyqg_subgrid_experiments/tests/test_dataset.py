b5129221cc95d4802423db69432ddfe530a16a55
Andrew Ross <andrewslavinross@gmail.com>
2021-12-10 08:50:25 -0500

    Update tests

--- pyqg_subgrid_experiments/tests/test_dataset.py
+++ pyqg_subgrid_experiments/tests/test_dataset.py
@@ -13,8 +13,10 @@ def load_dataset():
 def test_extract_feature():
     ds = load_dataset()
 
-    du_dx = ds.extract_feature('ddx_u')
-    div_u = ds.extract_feature('ddx_u_plus_ddy_v')
+    du_dx = ds.extract_feature('ddx(u)')
+    div_u = ds.extract_feature('add(ddx(u),ddy(v))')
+    div_u2 = ds.div('u','v')
 
     np.testing.assert_allclose(du_dx, ds.ddx('u'))
     np.testing.assert_allclose(div_u, ds.ddx('u') + ds.ddy('v'))
+    np.testing.assert_allclose(div_u, div_u2)
